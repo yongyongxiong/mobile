6fe8e85a9408156b9fc61fc1b6d31b320c3285f5
yongyongxiong <17319696963@163.com>
2018-11-13 18:15:08 +0800

    first commit

---
+++ js/public.js
@@ -0,0 +1,15 @@
+/*
+	$("#footer a").click(function(){
+		$(this).children(".iconfont").css({
+			"color":"#007aff",
+		})
+		$(this).children("p").css({
+			"color":"#007aff",
+		})
+		$(this).parent().siblings().children().children(".iconfont").css({	 			
+ 			"color":"#ababab",
+ 	})
+		$(this).parent().siblings().children().children("p").css({	 			
+ 			"color":"#ababab",
+ 		})
+})*/
